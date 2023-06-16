9a82f2f927c666ee7c96135f3568c33d7e2b315a
Prince-afk119 <Princephiri416@gmail.com>
2023-06-16 14:41:33 -0700

    task5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 /**
- *
  * main Entry point
  *
- * Return: Always 0. (success)
+ * Return: Always 0. (Success)
  */
 int main (void)
 {
-	printf("with proper gramar,but the outcome is a piece of art,\n");
+	printf("with proper gramar, but the outcome is a piece of art,\n");
 	return (0);
 }
