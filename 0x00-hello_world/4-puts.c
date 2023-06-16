2024afcf8352f77b48121aeda5cfb256df642d7c
Prince-afk119 <Princephiri416@gmail.com>
2023-06-16 14:26:28 -0700

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
-
 /**
- * main print "Programming is like building a multilingual
- *  puzzle, followed by a new line."
+ * main Entry point
+ *
  *  Return: Always 0. (success)
  */
 int main(void)
-
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
