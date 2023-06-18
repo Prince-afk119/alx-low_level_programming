3589dc74c7646817950370c1437e8c2a4f2a0b5a
Prince-afk119 <Princephiri416@gmail.com>
2023-06-18 14:38:03 -0700

    1-f

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int k;
+	char l;
+
+	for(k = 0 ; k < 10 ; k++)
+	{
+		putchar(k + '0');
+	}
+	for(l = 'a' ; l <= 'f' ; l++)
+	{
+		putchar(l);
+	}
+	putchar('\n');
+	return (0);
+}
