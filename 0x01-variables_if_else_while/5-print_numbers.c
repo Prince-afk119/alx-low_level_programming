34138170444758da71efd6c834f300496461cc00
Prince-afk119 <Princephiri416@gmail.com>
2023-06-18 14:01:28 -0700

    1-9

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -8,8 +8,8 @@ int main(void)
 {
 	int k;
 
-		for (k = 0 ; k < 10 ; k++)
-			printf ("%d" , k);
-		printf ("\n");
-		return (0);
+		for(k = 0 ; k < 10 ; k++)
+			printf("%d" , k);
+		printf("\n");
+		return(0);
 }
