19249029540dabc0ea01651fcf5af04b8c2db58f
Prince-afk119 <Princephiri416@gmail.com>
2023-06-18 15:14:02 -0700

    1,2,3

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return; Always 0
+ */
+int main(void)
+{
+	int k;
+
+	for (k = 0 ; k < 10 ; k++)
+	{
+		putchar (k + '0');
+		putchar (',');
+		putchar (' ');
+	}
+	return (0);
+}
