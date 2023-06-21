ff7d9ef1ebd32e9a818c665f24ae04728f3147f3
Prince-afk119 <Princephiri416@gmail.com>
2023-06-21 14:37:04 -0700

    1110

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,4 +4,9 @@
  * Return: 1  if c is uppercase or lowercase, otherwise 0
  */
 int _isalpha(int c)
-	
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else 
+		return (0);
+}
