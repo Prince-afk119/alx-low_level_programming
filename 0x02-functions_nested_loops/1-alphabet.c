d8ba1f24feaf29fca73a313493b5c2a421cbba01
Prince-afk119 <Princephiri416@gmail.com>
2023-06-21 07:19:41 -0700

    a-xz

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char k;
+
+	for (k = 'a' ; k <= 'z' ; k++)
+	{
+		_putchar(k);
+	}
+	_putchar('\n');
+}
