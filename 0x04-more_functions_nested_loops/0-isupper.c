72f41c9955fd45ed787e9525fe3930ff3395820a
Prince-afk119 <Princephiri416@gmail.com>
2023-06-22 12:57:46 -0700

    yoh

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,13 +1,18 @@
 #include "main.h"
+
 /**
- * main - Entry point
- * Return: 1 if uppercase, otherwise 0
+ * _isupper - uppercase letters
+ * @c: char to check
+ *
+ * Return: 0 or 1
  */
+
 int _isupper(int c)
 {
-	if( c >= 65 && c <= 90)
-	}
+	if (c >= 'A' && c <= 'Z')
 		return (1);
-	}
+	else
 		return (0);
 }
+
+
