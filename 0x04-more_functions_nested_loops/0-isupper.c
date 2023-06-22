86ffcc2330b267a2fd568ba9e95c398c24a38b1d
Prince-afk119 <Princephiri416@gmail.com>
2023-06-22 07:03:02 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Return: 1 is c is lowercase, otherwise 0
+ */
+int _isupper(int c)
+{
+	if ( c >= 'A' && c <= 'Z')
+	}
+		return (1);
+	}
+		return (0);
+}
