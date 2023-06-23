82e39ff78337770fcf17d01fe0303b21a34aa52b
Prince-afk119 <Princephiri416@gmail.com>
2023-06-23 05:26:21 -0700

    1,3,5

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+	char k;
+
+	for (k = '0'; k <= '9'; k++)
+	{
+		if (k != '2' && k !='4')
+		_putchar(k);
+	}
+	_putchar('\n');
+}
