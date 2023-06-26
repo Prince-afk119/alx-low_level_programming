84ee0dd001f80d54050617f411ea5e1b5e4484e0
Prince-afk119 <Princephiri416@gmail.com>
2023-06-26 12:45:13 -0700

    all done

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	 int m;
+
+
+        m = *a;
+
+        *a = *b;
+
+        *b = m;
+
+}
